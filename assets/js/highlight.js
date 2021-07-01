7b54c87ba075b83d707bb3ec431a06faf1422e6e
Henk Verlinde <henk@ventizo.com>
2021-07-01 16:37:59 +0200

    feat: update mermaid code fences usage

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -16,7 +16,7 @@ hljs.registerLanguage('toml', ini);
 hljs.registerLanguage('yaml', yaml);
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('pre code').forEach((block) => {
+  document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
     hljs.highlightElement(block);
   });
 });
